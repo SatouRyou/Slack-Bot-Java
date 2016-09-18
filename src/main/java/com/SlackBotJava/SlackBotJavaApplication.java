edf4e8111d5d9a93875935e1c3a32297afd8f906
SatouRyou <satouryou1205@gmail.com>
2016-09-18 23:46:17 +0900

    プロジェクトコミット

---
+++ src/main/java/com/SlackBotJava/SlackBotJavaApplication.java
@@ -0,0 +1,12 @@
+package com.SlackBotJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackBotJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackBotJavaApplication.class, args);
+	}
+}
